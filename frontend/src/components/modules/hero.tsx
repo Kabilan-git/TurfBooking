88f4083d3b4c39dd48a18e52326a059ccc11e4b1
Kabilan <kabilanraja7@gmail.com>
2025-11-02 17:04:49 +0530

    Try to remove DS_store- Kabilan

--- frontend/src/components/modules/hero.tsx
+++ frontend/src/components/modules/hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "../ui/button";
 
 const Hero = () => {
-  return <div className="p-5"></div>;
+  return <div className="p-5">Hero Page</div>;
 };
 
 export default Hero;
