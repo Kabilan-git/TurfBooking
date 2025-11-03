39b3567d4689b543152ad06f0818756c100a2ce2
Kabilan <kabilanraja7@gmail.com>
2025-11-04 02:27:23 +0530

    feat(page) : add hero page - Kabilan 🚀

--- frontend/src/components/modules/hero.tsx
+++ frontend/src/components/modules/hero.tsx
@@ -1,8 +1,47 @@
 import React from "react";
 import { Button } from "../ui/button";
+import Image from "next/image";
+import SearchGround from "./search-ground";
 
 const Hero = () => {
-  return <div className="p-5">Hero Page</div>;
+  return (
+    <div>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "700px",
+        }}
+      >
+        <Image
+          src="/hero.jpg"
+          alt="hero"
+          layout="fill"
+          objectFit="cover"
+          className="brightness-50"
+        ></Image>
+        <div className="absolute inset-0 flex justify-center items-center flex-col gap-4">
+          <h1 className="text-white text-6xl md:text-8xl">Find your perfect</h1>
+          <span className="text-turf-green text-6xl md:text-8xl font-medium">
+            Sports Turf
+          </span>
+          <span className="text-white text-xl max-w-sm md:max-w-screen font-light">
+            Book premium sports facilities, connect with players, and elevate
+            your game
+          </span>
+          <div className="action-btns space-x-5 mt-5">
+            <Button className="btn p-6 text-white bg-transparent border border-white">
+              Sign Up
+            </Button>
+            <Button className="btn p-6 bg-turf-green">Sign In</Button>
+          </div>
+        </div>
+      </div>
+      <div className="relative flex justify-center items-center">
+        <SearchGround></SearchGround>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
---
+++ frontend/src/components/modules/search-ground.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Calendar, MapPin, MoveRight } from "lucide-react";
+import Calendar22 from "@/components/shared/datepicker";
+import Dropdown from "@/components/shared/dropdown";
+
+const SearchGround = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+  const handleDateSelect = (date: Date | undefined) => {
+    setSelectedDate(date);
+    console.log(date);
+  };
+
+  const [selectedValue, setSelectedValue] = useState<String>("");
+  const handleSelectedValue = (value: string) => {
+    setSelectedValue(value);
+    console.log(value);
+  };
+
+  return (
+    <div className="min-h-max p-4 md:p-6 bg-white absolute rounded-xl">
+      <div>
+        <h1 className="text-[#317827] text-2xl font-semibold">
+          Search your play ground
+        </h1>
+        <div className="flex items-center gap-5 mt-3 md:flex-row flex-col">
+          <Input placeholder="Search location" className="md:max-w-max" />
+          <Button
+            className="btn text-neutral-900 bg-transparent border md:max-w-max w-sm"
+            variant="default"
+          >
+            <MapPin />
+            Near By
+          </Button>
+          <Calendar22 onDateSelect={handleDateSelect}></Calendar22>
+          <Dropdown onSelectedValue={handleSelectedValue}></Dropdown>
+          <Button className="btn bg-turf-green md:max-w-max">
+            Find Turf
+            <MoveRight />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchGround;
